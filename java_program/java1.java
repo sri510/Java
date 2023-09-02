bfc2b21da8ce28d3b13d78309be4c93196a9db2a
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-02 22:56:14 +0530

    Create java1.java

---
+++ java_program/java1.java
@@ -0,0 +1,27 @@
+/*
+Java Program to Check if a Given Integer is Odd or Even
+Input : 13
+Output: ODD
+Input : 24
+Output: EVEN
+*/
+import java.io.*;
+import java.util.Scanner;
+
+class GFG {
+
+	public static void main(String[] args)
+	{
+		int num = 10;
+
+		if (num % 2 == 0) {
+
+			System.out.println("Entered Number is Even");
+		}
+
+		else {
+
+			System.out.println("Entered Number is Odd");
+		}
+	}
+}
