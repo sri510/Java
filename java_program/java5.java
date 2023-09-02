657005bb6490b079a434d201b6f90de6508b159e
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-02 23:18:34 +0530

    Create java5.java

---
+++ java_program/java5.java
@@ -0,0 +1,13 @@
+/*
+The String class replace() method replaces all occurrence of first sequence of character with second sequence of character.
+Output - Kava is a programming language. Kava is a platform. Kava is an Island.
+*/
+public class Stringoperation8  
+{  
+public static void main(String ar[])  
+{  
+String s1="Java is a programming language. Java is a platform. Java is an Island.";      
+String replaceString=s1.replace("Java","Kava");     
+System.out.println(replaceString);    
+}  
+} 
