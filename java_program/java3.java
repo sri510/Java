2107c95dd9026f82eed4ebaabaed17dc30eb3984
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-02 23:12:41 +0530

    Create java3.java

---
+++ java_program/java3.java
@@ -0,0 +1,14 @@
+/*
+String s=new String("Welcome");//creates two objects and one reference variable.
+*/
+public class StringExample{  
+public static void main(String args[]){  
+String s1="Java";  
+char ch[]={'W','E','L','C','E','M','E'};  
+String s2=new String(ch);  
+String s3=new String("NIT");  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}
+}
