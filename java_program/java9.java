2fc08cb3ec769b30263c9fcc6a59f78eef0edda2
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-03 22:06:02 +0530

    Create java9.java

---
+++ java_program/java9.java
@@ -0,0 +1,21 @@
+/*
+Write a Java program to display the following pattern.
+Sample Pattern :
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+*/
+package My_Work;
+
+public class Java9 {
+	
+	    public static void main(String args[]) {
+
+	        System.out.println("   J    a   v     v  a ");
+	        System.out.println("   J   a a   v   v  a a");
+	        System.out.println("J  J  aaaaa   V V  aaaaa");
+	        System.out.println(" JJ  a     a   V  a     a");
+	    }
+	}
