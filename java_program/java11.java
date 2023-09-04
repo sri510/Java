3af73f1b62b9c33aaa58f6b95b30e71291e6e6b9
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-04 22:53:42 +0530

    Create java11.java

---
+++ java_program/java11.java
@@ -0,0 +1,16 @@
+/* Write a program to find out the area of Rectangle where length and breadth of the Rectangle must
+   be taken from user. */
+
+package Sep_04;
+
+public class AreaOfRectangle {
+	public static void main(String[] args)
+	{
+		
+		int l=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int a;
+		 a=l*b;
+		System.out.println("Area of Rectangle: "+a);
+		}
+}
