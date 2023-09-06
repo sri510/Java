edcea3be619b18986740d057d01eb0c0b931b4cb
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-09-06 09:51:23 +0530

    Create java18.java

---
+++ java_program/java18.java
@@ -0,0 +1,16 @@
+/*
+Write a Java program and compute the sum of a two digit integer number
+Input = 54
+Output = 9
+*/
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=4;
+        System.out.println("Input = "+a + b);
+        
+        System.out.println("Output = "+(a+b));
+    }
+}
